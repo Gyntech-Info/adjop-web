cdaa29a043b6a5edeb6d5d4cb328a9a45c69a141
gleysonareas <gleysonareasdasilva@hotmail.com>
2024-02-21 19:10:20 -0300

    update project move components to shared directory

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,21 +1,11 @@
 import { NgModule } from '@angular/core';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { DescriptionComponent } from './description/description.component';
-import { NewsComponent } from './news/news.component';
-import { VideosComponent } from './videos/videos.component';
 
 @NgModule({
   imports: [
-    AboutUsComponent,
-    DescriptionComponent,
-    NewsComponent,
-    VideosComponent,
+
   ],
   exports: [
-    AboutUsComponent,
-    DescriptionComponent,
-    NewsComponent,
-    VideosComponent,
+
   ],
 })
 export class ComponentsModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,29 @@ import { NgModule } from '@angular/core';
 import { BannerComponent } from "./components/banner/banner.component";
 import { CarouselComponent } from "./components/carousel/carousel.component";
 import { TabComponent } from "./components/tab/tab.component";
+import { AboutUsComponent } from "./components/about-us/about-us.component";
+import { DescriptionComponent } from "./components/description/description.component";
+import { NewsComponent } from "./components/news/news.component";
+import { VideosComponent } from "./components/videos/videos.component";
 
 @NgModule({
   imports: [
     BannerComponent,
     CarouselComponent,
-    TabComponent
+    TabComponent,
+    AboutUsComponent,
+    DescriptionComponent,
+    NewsComponent,
+    VideosComponent,
   ],
   exports: [
     BannerComponent,
     CarouselComponent,
-    TabComponent
+    TabComponent,
+    AboutUsComponent,
+    DescriptionComponent,
+    NewsComponent,
+    VideosComponent,
   ]
 })
 export class SharedModule { }
